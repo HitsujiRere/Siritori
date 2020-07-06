f0d9e54a95d0e3309a281998371ba6696f2755dd
HitsujiRere <hitsujirere@gmail.com>
2020-07-06 21:43:22 +0900

    Fix UI.

--- js/siritori.js
+++ js/siritori.js
@@ -10,7 +10,7 @@ function confirmed_my_word() {
 
 function check_words() {
     var my_word = document.getElementById("my_word_input").value;
-    var back_words_head = document.getElementById("back_words_head").innerHTML;
+    var back_words_head = document.getElementById("back_word_head").innerHTML;
 
     console.log(back_words_head + " -> " + my_word);
     console.log(back_words_head.slice(-1) + " -> " + my_word.slice(0, 1));
@@ -27,14 +27,18 @@ function add_words() {
     var my_word = document.getElementById("my_word_input").value;
     var my_word_el = document.createTextNode(my_word);
 
+    document.getElementById("backest_word").innerHTML = my_word;
+
     var new_back_word_el = document.createElement("li");
     new_back_word_el.appendChild(my_word_el);
 
-    var back_words_head_el = document.getElementById("back_words_head");
+    var back_words_head_el = document.getElementById("back_word_head");
 
     var back_words_el = document.getElementById("back_words");
     back_words_el.insertBefore(new_back_word_el, back_words_head_el);
 
     back_words_head_el.id = "";
-    new_back_word_el.id = "back_words_head";
+    new_back_word_el.id = "back_word_head";
+
+    document.getElementById("my_word_input").value = "";
 }
