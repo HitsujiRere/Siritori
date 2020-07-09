2f1577138b174b26f3d6427f4abd075664b3de5d
HitsujiRere <hitsujirere@gmail.com>
2020-07-09 21:42:27 +0900

    Add comments to siritori_1.js.

--- js/siritori_1.js
+++ js/siritori_1.js
@@ -1,48 +1,68 @@
+
+// 以前使用した単語たち
 const backWords = new Set();
 
-backWords.add("しりとり");
-
+// 繋がった回数
 let connectTime = 0;
 
+// メッセージテキスト
 let messageText = "";
 
 window.onload = function () {
+    // 最初の単語の設定
+    backWords.add("しりとり");
+
+    // ツイートボタンの初期化
     updateTweetText();
 };
 
+// 単語の入力を感知する
 function confirmedMyWord() {
+    const myWord = document.getElementById("my_word_input").value;
+
+    // 単語が入力されていないなら終わる
+    if (myWord == "") {
+        return false;
+    }
+
+    // 入力した単語が正しいか調べる
     if (checkWord()) {
+        // 単語の追加
         addWord();
+        // ツイートする文の更新
         updateTweetText();
     }
 
+    // メッセージの更新
     updateMessage();
 
     return false;
 }
 
+// 入力された単語が正しいか確かめる
 function checkWord() {
-    const myWord = document.getElementById("my_word_input").value;
-    if (myWord == "")
-        return false;
-
-    if (checkHiragana() && checkConnect() && checkUsed()) {
+    if (checkHiragana() && checkConnect() && checkNotUsed()) {
         console.log("OK!");
+
         connectTime++;
         messageText = `${connectTime}個目！`;
+
         return true;
     }
     else {
         console.log("NO!");
+
         return false;
     }
 }
 
+// 全てひらがなかどうか
 function checkHiragana() {
     const myWord = document.getElementById("my_word_input").value;
 
     if (myWord == null) {
         messageText = "空文字です";
+
         return false;
     }
 
@@ -50,18 +70,20 @@ function checkHiragana() {
         return true;
     } else {
         messageText = "ひらがなではありません";
+
         return false;
     }
 }
 
+// 前の単語と繋がっているか
 function checkConnect() {
     const myWord = document.getElementById("my_word_input").value;
-    const backestWord = document.getElementById("backest_word").innerHTML;
+    const backWord = document.getElementById("backest_word").innerHTML;
 
-    console.log(backestWord + " -> " + myWord);
-    console.log(backestWord.slice(-1) + " -> " + myWord.slice(0, 1));
+    console.log(backWord + " -> " + myWord);
+    console.log(backWord.slice(-1) + " -> " + myWord.slice(0, 1));
 
-    if (backestWord.slice(-1) == myWord.slice(0, 1)) {
+    if (backWord.slice(-1) == myWord.slice(0, 1)) {
         return true;
     } else {
         messageText = "前の単語と繋がりません";
@@ -69,7 +91,8 @@ function checkConnect() {
     }
 }
 
-function checkUsed() {
+// 使われていないか調べる
+function checkNotUsed() {
     const myWord = document.getElementById("my_word_input").value;
 
     if (!backWords.has(myWord)) {
@@ -80,10 +103,11 @@ function checkUsed() {
     }
 }
 
+// 単語を追加する
 function addWord() {
     const myWord = document.getElementById("my_word_input").value;
 
-    addWordToUl();
+    addWordToBackWords();
 
     document.getElementById("backest_word").innerHTML = myWord;
 
@@ -92,7 +116,8 @@ function addWord() {
     backWords.add(myWord);
 }
 
-function addWordToUl() {
+// 過去への単語の追加
+function addWordToBackWords() {
     const myWord = document.getElementById("my_word_input").value;
     const myWordElement = document.createTextNode(myWord);
 
@@ -103,21 +128,23 @@ function addWordToUl() {
     backWordsElement.insertBefore(newBackWordElement, backWordsElement.firstChild);
 }
 
+// ツイートするテキストの変更
 function updateTweetText() {
     setTweetButton(
         `１人しりとりで${connectTime}回続きました！`
     );
 }
 
+// メッセージの更新
 function updateMessage() {
     const messageEl = document.getElementById("input_message");
     messageEl.innerHTML = messageText;
 }
 
-// 任意のタイミングで呼べば狙ったとおりのテキストのボタンつくれる
-// 引数増やしていろいろやってもよいですね。
 // FROM: https://qiita.com/lovesaemi/items/d4f296b6b1d5158d2fea
 // FIX : URLを任意のものに変更できない
+// 任意のタイミングで呼べば狙ったとおりのテキストのボタンつくれる
+// 引数増やしていろいろやってもよいですね。
 function setTweetButton(text) {
     //$('#tweet-area').empty(); //既存のボタン消す
     document.getElementById("tweet-area").textContent = null;
